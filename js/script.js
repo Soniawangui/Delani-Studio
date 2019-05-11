2280dc60d48f08aad4c2d817afc5cf9c225214b5
Soniawangui <koisonia99@gmail.com>
2019-05-11 08:51:24 +0300

    Add p tags to the portfolio section

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
+$(document).ready(function(){
+  $(".design").click(function(){
+    $(".design").hide();
+    $(".designp").toggle();
+    $(".designp").show();
+  })
+})
+
+$(".designp").click(function(){
+  $(".design").hide();
+  $(".designp").toggle();
+  $(".design").show();
+})
+$(".development").click(function(){
+  $(".development").hide();
+  $(".developmentp").toggle();
+  $(".developmentp").show();
+})
+
+$(".developmentp").click(function(){
+  $(".development").hide();
+  $(".developmentp").toggle();
+  $(".development").show();
+})
+
+$(".product-management").click(function(){
+  $(".product-management").hide();
+  $(".product-managementp").toggle();
+  $(".product-managementp").show();
+  })
 
 
+$(".product-managementp").click(function(){
+  $(".product-management").hide();
+  $(".product-managementp").toggle();
+  $(".product-management").show();
+
+})
